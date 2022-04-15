6b6b70b06fca256fe01b18195b5725779705e24e
Deepak Kumar <deepak.kumar07@nagarro.com>
2022-04-15 18:19:19 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,46 @@
+let a = 5;
+console.log('a - ',a);
+
+// data types - 
+// Number, string, Boolean
+// object - array
+
+let num1 = 5;
+let num2 = 6;
+
+function add(a,b) {
+    return a + b;
+}
+
+console.log(add(num1, num2));
+
+
+// function, loops, if else, switch
+
+// DOM - Document Object Model
+
+
+
+let obj = {
+    name: 'asiuf',
+    address: 'iwued',
+    state: 'osif'
+}
+
+let document = {
+    head: {
+        title: 'kaefh',
+    },
+    body: {
+        div_container: {
+            div_header : '<div>Header</div>',
+            div_main : '<div>Main</div>',
+            div_footer : '<div>Footer</div>'
+        }
+    }
+}
+
+
+// window.document
+// document
+// Browser.HTML
